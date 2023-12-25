bda98ad2179596847bdb2b5290e86efeddaeb030
AbhilashGogoi10 <abhilashgogoi33@gmail.com>
2023-12-25 12:35:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
+import { useState } from "react";
+
+function Cell({ filled, onClick, isDisabled, label }) {
+  return (
+    <button
+      type="button"
+      aria-label={label}
+      isDisabled={isDisabled}
+      onClick={onClick}
+      className={filled ? "cell cell-activated" : "cell"}
+    />
+  );
+}
+
 
 function App() {
+  const [order, setOrder] = useState([]);
+  const [isDeactivating, setIsDeactivating] = useState(false);
+
+  const config = [
+    [1, 1, 1],
+    [1, 0, 1],
+    [1, 1, 1],
+  ];
+
+  
+const deactiveCells = ()=>{
+  setIsDeactivating(true);
+  const timer = setInterval(() => {
+    setOrder((oriOrder)=>{
+      const newOrder = oriOrder.slice();
+      newOrder.pop();
+  
+      if(newOrder.length === 0){
+        clearInterval(timer);
+        setIsDeactivating(false);
+      }
+  
+      return newOrder;
+
+    })
+   
+  }, 300);
+}
+
+const activateCells = (index) => {
+  const newOrder = [...order, index];
+  setOrder(newOrder);
+
+
+  if(newOrder.length === config.flat(1).filter(Boolean).length){
+    deactiveCells();
+  }
+};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <div className="grid" style={{gridTemplateColumns:`repeat(${config[0].length}, 1fr)`,}}>
+        {config.flat(1).map((value, index) => {
+          return (
+            value? ( <Cell
+            label={`Cell ${index}`}
+              key={index}
+              filled={order.includes(index)}
+              isDisabled = {order.includes(index) || isDeactivating}
+              onClick={() => activateCells(index)}
+            /> )
+           : (<span/>)
+          );
+        })}
+      </div>
     </div>
   );
 }
